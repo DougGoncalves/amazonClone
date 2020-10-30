b550abb9b37a432f119e1897f984c87b68306e8b
DougGoncalves <doug.goncalvesf@gmail.com>
2020-10-30 10:10:01 -0300

    payment page

--- src/App.js
+++ src/App.js
@@ -4,37 +4,41 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import Checkout from './pages/Checkout'
 import Login from './pages/Login'
+import Payment from './pages/Payment'
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import { auth } from './firebase'
-import { useStateValue } from './services/StateProvider';
+import {useStateValue} from './services/StateProvider'
+import {auth} from './firebase'
 
 function App() {
-  const[{}, dispatch] = useStateValue;
+const [{}, dispatch] = useStateValue();
 
-  useEffect(() => {
-    auth.onAuthStateChanged(authUser => {
-      console.log('THE USER IS >>>', authUser);
+useEffect(() => {
 
-      if (authUser) {
-        dispatch({
-          type: 'SET_USER',
-          user: authUser
-        })
-      } else {
-        dispatch({
-          type: 'SET_USER',
-          user: null
-        })
-      }
-    })
-  }, [])
+  auth.onAuthStateChanged(authUser => {
+    console.log('THE USER IS >>>', authUser);
+
+    if(authUser) {
+      dispatch({
+        type: 'SET_USER',
+        user: authUser
+      })
+    } else {
+      dispatch({
+        type: 'SET_USER',
+        user: null
+      })
+    }
+  })
+}, [])
 
   return (
     <Router>
       <div className="app">
-      
         <Switch>
-          
+          <Route path="/payment">
+            <Header/>
+            <Payment/>
+          </Route>
           <Route path="/login">
             <Login/>
           </Route>
